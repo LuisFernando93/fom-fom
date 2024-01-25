a7759793a92f7e6f23b5a08f44b82fb208138e33
LuisFernando93 <39248747+LuisFernando93@users.noreply.github.com>
2024-01-25 00:13:56 -0300

    Audio support implemented. SoundManager prefab updated. Options Menu sound control integrated with SoundManager

--- Assets/Scripts/SoundManager/SoundManager.cs
+++ Assets/Scripts/SoundManager/SoundManager.cs
@@ -32,4 +32,40 @@ public class SoundManager : MonoBehaviour
         _musicSource.clip = clip;
         _musicSource.Play();
     }
+
+    public void ChangeMasterVolume(float value)
+    {
+        _mixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
+    }
+
+    public void ChangeMusicVolume(float value)
+    {
+        _mixer.SetFloat("VolumeMusic", Mathf.Log10(value) * 20);
+    }
+
+    public void ChangeSFXVolume(float value)
+    {
+        _mixer.SetFloat("VolumeSFX", Mathf.Log10(value) * 20);
+    }
+
+    public float GetVolumeFromMixer(string type)
+    {
+        switch (type)
+        {
+            case "master":
+                _mixer.GetFloat("MasterVolume", out float volumeMaster);
+                volumeMaster = Mathf.Pow(10f, volumeMaster / 20);
+                return volumeMaster;
+            case "music":
+                _mixer.GetFloat("VolumeMusic", out float volumeMusic);
+                volumeMusic = Mathf.Pow(10f, volumeMusic / 20);
+                return volumeMusic;
+            case "SFX":
+                _mixer.GetFloat("VolumeSFX", out float volumeSFX);
+                volumeSFX = Mathf.Pow(10f, volumeSFX / 20);
+                return volumeSFX;
+            default:
+                return 1f;//String incompativel
+        }
+    }
 }
