44cbddeb8b15cfc12a16df7959625bba83e4bade
LuisFernando93 <39248747+LuisFernando93@users.noreply.github.com>
2024-01-25 21:33:14 -0300

    Card, CardType and CardTemplate scripts added

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,15 @@
+public class Card
+{
+    private string cardName;
+    private int cardId;
+    private CardType cardType;
+
+    public Card(string cardName, int cardId, CardType cardType)
+    {
+        this.cardName = cardName;
+        this.cardId = cardId;
+        this.cardType = cardType;
+    }
+
+    
+}
