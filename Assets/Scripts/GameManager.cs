531d69f9d5bf466fc281b23be7e5fbc0252e84a8
LuisFernando93 <39248747+LuisFernando93@users.noreply.github.com>
2024-01-27 20:23:01 -0300

    Game Over and Victory scenes added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -9,18 +10,23 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject playerHandUI;
     [SerializeField] private GameObject showSelectedCardsUI;
     [SerializeField] private GameObject playerDisplay, enemyDisplay;
+    [SerializeField] private GameObject player, enemy;
+    [SerializeField] private GameObject popularityBar;
     private Card enemyCard, playerCard;
     private string phase;
+    private int round;
     private bool isShowing = false;
 
     void Start()
     {
         SoundManager.Instance.PlayMusic(music);
+        round = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        checkRound();
         phase = BattlePhaseManager.Instance.getCurrentPhase();
         switch (phase)
         {
@@ -36,16 +42,22 @@ public class GameManager : MonoBehaviour
                 break;
             case "animate":
                 playerHandUI.SetActive(false);
+                BattlePhaseManager.Instance.nextPhase();
                 break;
             case "score":
                 playerHandUI.SetActive(false);
+                BattlePhaseManager.Instance.nextPhase();
                 break;
             case "redraw":
                 playerHandUI.SetActive(false);
+                round++;
+                Debug.Log("Round: " + round);
+                BattlePhaseManager.Instance.nextPhase();
                 break;
             default:
                 Debug.Log("Erro na transicao de turno");
                 break;
+            
         }
     }
 
@@ -70,5 +82,25 @@ public class GameManager : MonoBehaviour
         BattlePhaseManager.Instance.nextPhase();
         isShowing = false;
         yield break;
+    }
+
+    private void redrawPhase()
+    {
+
+    }
+
+    private void checkRound()
+    {
+        if (round > 5)
+        {
+            int score = popularityBar.GetComponent<Popularity>().getPopularityScore();
+            if(score > 0)
+            {
+                SceneManager.LoadScene("Victory");
+            } else
+            {
+                SceneManager.LoadScene("GameOver");
+            }
+        }
     }
 }
